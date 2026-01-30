9311b5c6f594ef647bea6acc64e372c6b72393bf
Akif <akifshah7@gmail.com>
2026-01-30 12:55:22 +0530

    temp changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.use(express.json());
 
+app.post("/", (req, res) => {
+  console.log("ROOT HIT");
+  res.sendStatus(200);
+});
+
 app.post("/webhook/github", (req, res) => {
   console.log("Webhook received");
   res.status(200).send("OK");
